8fe4521b53f9c369daa39a3a55eec60a1e6d4887
Prajapati Aryan <ap5489586@gmail.com>
2026-05-25 11:25:28 +0530

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://aryanprjportfolio.vercel.app/',
-    ],
-    credentials: true,
-  });
+  origin: [
+    'http://localhost:3000',
+    'https://aryanprjportfolio.vercel.app',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  credentials: true,
+});
 
   await app.listen(process.env.PORT ?? 3001,"0.0.0.0");
 }
